3b08e7b106599aabb7d6caaf54ef454ce2e7bcf8
David Schramm <david.schramm@live.com>
2022-07-19 22:29:37 +0200

    uss wchar_t format specifier

--- wulimit.c
+++ wulimit.c
@@ -14,18 +14,18 @@
 void print_last_error_message(void){
 	wchar_t *emsg=NULL;
 
-	if(!FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_FROM_SYSTEM, NULL, GetLastError(), 0, (char *) &emsg, 0, NULL))
+	if(!FormatMessageW(FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_FROM_SYSTEM, NULL, GetLastError(), 0, (wchar_t *) &emsg, 0, NULL))
         fwprintf(stderr, L"[print_last_error_message] failed getting error message!\n");
     else
-        fwprintf(stderr, L"GetLastError(): %s\n", emsg);
+        fwprintf(stderr, L"GetLastError(): %ls\n", emsg);
     LocalFree(emsg);
 }
 
-PTOKEN_USER get_ptoken_user(HANDLE proc_handle, PTOKEN_USER ptoken_user, unsigned long *token_user_size) {
+PTOKEN_USER get_token_user(HANDLE proc_handle, PTOKEN_USER token_user, unsigned long *token_user_size) {
     HANDLE token_handle=NULL;
 
     if(!OpenProcessToken(proc_handle, TOKEN_READ, &token_handle)) {
-        fprintf(stderr, "[get_ptoken_user] failed getting process token!\n");
+        fprintf(stderr, "[get_token_user] failed getting process token!\n");
         goto fail;
     }
 
@@ -33,20 +33,20 @@ PTOKEN_USER get_ptoken_user(HANDLE proc_handle, PTOKEN_USER ptoken_user, unsigne
 
     GetTokenInformation(token_handle, TokenUser, NULL, 0, &needed_token_user_size);
 
-    if((!ptoken_user)||*token_user_size<needed_token_user_size) {
-        free(ptoken_user);
-        ptoken_user=malloc(needed_token_user_size);
+    if((!token_user)||*token_user_size<needed_token_user_size) {
+        free(token_user);
+        token_user=malloc(needed_token_user_size);
     }
 
-    if(!GetTokenInformation(token_handle, TokenUser, ptoken_user, needed_token_user_size, token_user_size)) {
-        fprintf(stderr, "[get_ptoken_user] failed getting token information!\n");
+    if(!GetTokenInformation(token_handle, TokenUser, token_user, needed_token_user_size, token_user_size)) {
+        fprintf(stderr, "[get_token_user] failed getting token information!\n");
         goto fail;
     }
 
     if(token_handle)
         CloseHandle(token_handle);
 
-    return ptoken_user;
+    return token_user;
 
 fail:
 
@@ -58,25 +58,25 @@ fail:
     return NULL;
 }
 
-uint8_t is_process_owner(PTOKEN_USER user_token, unsigned long pid){
+uint8_t is_process_owner(PTOKEN_USER token_user, unsigned long pid){
 	HANDLE proc_handle=NULL;
-    PTOKEN_USER proc_ptoken_user=NULL;
-    unsigned long proc_ptoken_user_size=0;
+    PTOKEN_USER proc_token_user=NULL;
+    unsigned long proc_token_user_size=0;
 
     if((proc_handle=OpenProcess(PROCESS_QUERY_INFORMATION|PROCESS_VM_READ, 0, pid))==NULL) {
 //        fprintf(stderr, "[is_process_owner] failed getting process handle!\n");
         goto fail;
     }
 
-    if((proc_ptoken_user=get_ptoken_user(proc_handle, proc_ptoken_user, &proc_ptoken_user_size))==NULL) {
+    if((proc_token_user=get_token_user(proc_handle, proc_token_user, &proc_token_user_size))==NULL) {
 //        fprintf(stderr, "[is_process_owner] failed getting user token!\n");
         goto fail;
     }
 	
-	uint8_t r=EqualSid(user_token->User.Sid, proc_ptoken_user->User.Sid);
+	uint8_t r=EqualSid(token_user->User.Sid, proc_token_user->User.Sid);
 
     CloseHandle(proc_handle);
-    free(proc_ptoken_user);
+    free(proc_token_user);
 
     return r;
 
@@ -87,17 +87,17 @@ fail:
     if(proc_handle)
         CloseHandle(proc_handle);
 
-    free(proc_ptoken_user);
+    free(proc_token_user);
 
     return 0;
 
 }
 
-uint8_t get_username_from_ptoken_user(PTOKEN_USER ptoken_user, wchar_t *username, unsigned long username_s,
+uint8_t get_username_from_ptoken_user(PTOKEN_USER token_user, wchar_t *username, unsigned long username_s,
 									wchar_t *domain, unsigned long domain_s){
 	
 	SID_NAME_USE sidNameUse;
-    if(!LookupAccountSidW(NULL, ptoken_user->User.Sid, username, &username_s, domain, &domain_s, &sidNameUse)) {
+    if(!LookupAccountSidW(NULL, token_user->User.Sid, username, &username_s, domain, &domain_s, &sidNameUse)) {
         fprintf(stderr, "failed looking up username!\n");
 		print_last_error_message();
 		return 0;	      
@@ -106,11 +106,11 @@ uint8_t get_username_from_ptoken_user(PTOKEN_USER ptoken_user, wchar_t *username
 	return 1;
 }
 
-uint8_t get_process_owner2(unsigned long pid, char *owner, unsigned long owner_s,
+uint8_t get_process_owner(unsigned long pid, char *owner, unsigned long owner_s,
                            char *domain, unsigned long domain_s) {
 
     HANDLE proc_handle=NULL;
-    PTOKEN_USER ptoken_user=NULL;
+    PTOKEN_USER token_user=NULL;
     unsigned long token_user_size=0;
 
     if((proc_handle=OpenProcess(PROCESS_QUERY_INFORMATION|PROCESS_VM_READ, 0, pid))==NULL) {
@@ -118,20 +118,20 @@ uint8_t get_process_owner2(unsigned long pid, char *owner, unsigned long owner_s
         goto fail;
     }
 
-    if((ptoken_user=get_ptoken_user(proc_handle, ptoken_user, &token_user_size))==NULL) {
+    if((token_user=get_token_user(proc_handle, token_user, &token_user_size))==NULL) {
         fprintf(stderr, "failed getting user token!\n");
         goto fail;
     }
 
     SID_NAME_USE sidNameUse;
-    if(!LookupAccountSidA(NULL, ptoken_user->User.Sid, owner, &owner_s, domain, &domain_s, &sidNameUse)) {
+    if(!LookupAccountSidA(NULL, token_user->User.Sid, owner, &owner_s, domain, &domain_s, &sidNameUse)) {
         fprintf(stderr, "failed loolking up account sid!\n");
         goto fail;
     }
 
     if(proc_handle)
         CloseHandle(proc_handle);
-    free(ptoken_user);
+    free(token_user);
     return 1;
 
 fail:
@@ -141,69 +141,7 @@ fail:
     if(proc_handle)
         CloseHandle(proc_handle);
 
-    free(ptoken_user);
-
-    return 0;
-}
-
-
-uint8_t get_process_owner(unsigned long pid, char *owner, unsigned long owner_s,
-                          char *domain, unsigned long domain_s) {
-
-    HANDLE proc_handle=NULL;
-    HANDLE token_handle=NULL;
-    PTOKEN_USER ptoken_user=NULL;
-    char *emsg=NULL;
-
-    if((proc_handle=OpenProcess(PROCESS_QUERY_INFORMATION|PROCESS_VM_READ, 0, pid))==NULL) {
-        fprintf(stderr, "failed getting process handle!\n");
-        goto fail;
-    }
-
-    if(!OpenProcessToken(proc_handle, TOKEN_READ, &token_handle)) {
-        fprintf(stderr, "failed getting process token!\n");
-        goto fail;
-    }
-
-    unsigned long token_user_length;
-
-    GetTokenInformation(token_handle, TokenUser, NULL, 0, &token_user_length);
-
-    ptoken_user=malloc(token_user_length);
-
-    if(!GetTokenInformation(token_handle, TokenUser, ptoken_user, token_user_length, &token_user_length)) {
-        fprintf(stderr, "failed getting token information!\n");
-        goto fail;
-    }
-
-    SID_NAME_USE sidNameUse;
-    if(!LookupAccountSidA(NULL, ptoken_user->User.Sid, owner, &owner_s, domain, &domain_s, &sidNameUse)) {
-        fprintf(stderr, "failed loolking up account sid!\n");
-        goto fail;
-    }
-
-    if(token_handle)
-        CloseHandle(token_handle);
-    if(proc_handle)
-        CloseHandle(proc_handle);
-    free(ptoken_user);
-    return 1;
-
-fail:
-
-    if(!FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_FROM_SYSTEM, NULL, GetLastError(), 0, (char *) &emsg, 0, NULL))
-        fprintf(stderr, "failed getting error message!\n");
-    else
-        fprintf(stderr, "GetLastError(): %s\n", emsg);
-    LocalFree(emsg);
-
-
-    if(token_handle)
-        CloseHandle(token_handle);
-    if(proc_handle)
-        CloseHandle(proc_handle);
-
-    free(ptoken_user);
+    free(token_user);
 
     return 0;
 }
@@ -234,7 +172,7 @@ uint8_t list_procs(void) {
     procs_size_used/=sizeof(unsigned long);
 
 	unsigned long current_user_token_size=0;
-	PTOKEN_USER current_user_token=get_ptoken_user(GetCurrentProcess(), NULL, &current_user_token_size);
+	PTOKEN_USER current_user_token=get_token_user(GetCurrentProcess(), NULL, &current_user_token_size);
 	
     wchar_t owner[_MAX_PATH], domain[_MAX_PATH];
 	wchar_t *sid;
@@ -242,13 +180,13 @@ uint8_t list_procs(void) {
 	get_username_from_ptoken_user(current_user_token, owner, _MAX_PATH, domain, _MAX_PATH);
 	ConvertSidToStringSidW(current_user_token->User.Sid, &sid);
 	
-	wprintf(L"Current SID: %s@%s (%s)\n", owner, domain, sid);
+	wprintf(L"Current SID: %ls@%ls (%ls)\n", owner, domain, sid);
 	LocalFree(sid);
 
 	
 	printf("num_processes: %ld\n", procs_size_used);	
     for(unsigned long i=0; i<procs_size_used; ++i) {
-		//get_process_owner2(procs[i], owner, _MAX_PATH, domain, _MAX_PATH);
+		//get_process_owner(procs[i], owner, _MAX_PATH, domain, _MAX_PATH);
 		if(is_process_owner(current_user_token, procs[i])){
             printf("%ld owned by current user\n", procs[i]);
     	}
